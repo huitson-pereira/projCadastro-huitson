a7f0ca81b166ad83ca07c0fa37c3e7f02ba90e4d
huitson-pereira <124617896+huitson-pereira@users.noreply.github.com>
2023-04-06 15:22:38 -0300

    Ex.7 - Criação de header em App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ function App() {
   
   return (
     <div className="App">
+      <header>
+        <h2>Cadastre-se gratuitamente!</h2>
+        <p>Faça o seu cadastro para ter acesso a todos os recursos da nossa plataforma</p>
+      </header>
       <div>
             <form>
                 {/*aqui vai a regra para exibir o componente do passo atual*/}
